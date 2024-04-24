fca0b1eea2ccb45c81fcefe6d201349e0db9417a
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-04-24 11:38:22 -0500

    :boom: Properly defined role and permission structures

---
+++ ManageFood.Contracts/Enums/Permissions.cs
@@ -0,0 +1,8 @@
+namespace ManageFood.Contracts.Enums
+{
+  public struct Permissions
+  {
+    public const string AllowManage = nameof(AllowManage);
+    public const string AllowOrders = nameof(AllowOrders);
+  }
+}
---
+++ ManageFood.Contracts/Enums/Roles.cs
@@ -0,0 +1,8 @@
+namespace ManageFood.Contracts.Enums
+{
+  public struct Roles
+  {
+    public const string AdminUser = nameof(AdminUser);
+    public const string CommonUser = nameof(CommonUser);
+  }
+}
