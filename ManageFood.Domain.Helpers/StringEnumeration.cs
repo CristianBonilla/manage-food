48f373a38cffe2b5d14137ae3f3bcad7d4c2a380
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-05-03 23:10:25 -0500

    :boom: Reading string enumeration helper has been improved

--- ManageFood.Domain.Helpers/StringEnumeration.cs
+++ ManageFood.Domain.Helpers/StringEnumeration.cs
@@ -4,15 +4,17 @@ namespace ManageFood.Domain.Helpers
 {
   public abstract class StringEnumeration : IComparable
   {
+    static readonly BindingFlags _bindingFlags = BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly;
+
     public int Key {  get; set; }
     public string Value { get; set; }
 
     protected StringEnumeration(int key, string value) => (Key, Value) = (key, value);
 
-    public static IEnumerable<T> GetAll<T>() where T : StringEnumeration =>
-      typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
-        .Select(field => field.GetValue(null))
-        .Cast<T>();
+    public static IEnumerable<T> GetAll<T>() where T : StringEnumeration => GetEnumerations<T>();
+
+    public static T? FromValue<T>(string value) where T : StringEnumeration =>
+      GetEnumerations<T>().FirstOrDefault(enumeration => enumeration.Value == value);
 
     public int CompareTo(object? other) => Key.CompareTo((other as StringEnumeration)?.Key ?? -1);
 
@@ -29,5 +31,22 @@ namespace ManageFood.Domain.Helpers
     }
 
     public override int GetHashCode() => Key.GetHashCode();
+
+    private static IEnumerable<T> GetEnumerations<T>() where T : StringEnumeration
+    {
+      Type type = typeof(T);
+      IEnumerable<MemberInfo> members = [.. type.GetFields(_bindingFlags), .. type.GetProperties(_bindingFlags)];
+      foreach (MemberInfo member in members)
+      {
+        object? memberValue = member switch
+        {
+          FieldInfo field => field.GetValue(null),
+          PropertyInfo property => property.GetValue(null),
+          _ => null
+        };
+        if (memberValue is T enumeration)
+          yield return enumeration;
+      }
+    }
   }
 }
