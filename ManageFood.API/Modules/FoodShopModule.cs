3b6472ea56970f7096f43fae5aadecc401ebcd75
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-06-29 19:26:23 -0500

    :boom: Order repository is created and injected with autofac

--- ManageFood.API/Modules/FoodShopModule.cs
+++ ManageFood.API/Modules/FoodShopModule.cs
@@ -17,6 +17,9 @@ namespace ManageFood.API.Modules
       builder.RegisterType<InventoryRepository>()
         .As<IInventoryRepository>()
         .InstancePerLifetimeScope();
+      builder.RegisterType<OrderRepository>()
+        .As<IOrderRepository>()
+        .InstancePerLifetimeScope();
     }
   }
 }
